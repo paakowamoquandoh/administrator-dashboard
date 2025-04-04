17623aae792b5862cc7d94a5cb778e78ce8f8a8d
felix amoquandoh <55555068+paakowamoquandoh@users.noreply.github.com>
2025-04-04 20:57:21 +0000

    app files added

---
+++ src/formSource.js
@@ -0,0 +1,233 @@
+export const userInputs = [
+  {
+    id: "username",
+    label: "Username",
+    type: "text",
+    placeholder: "john noble",
+  },
+  {
+    id: "email",
+    label: "Email",
+    type: "email",
+    placeholder: "john_doe@gmail.com",
+  },
+  {
+    id: "phone",
+    label: "Phone",
+    type: "text",
+    placeholder: "+233 234 567 89",
+  },
+  {
+    id: "password",
+    label: "Password",
+    type: "password",
+  },
+  {
+    id: "address",
+    label: "Address",
+    type: "text",
+  },
+  {
+    id: "country",
+    label: "Country",
+    type: "text",
+    placeholder: "Ghana",
+  },
+  {
+    id: "idNumber",
+    label: "Ghana-Card Number",
+    type: "text",
+    placeholder: "GHA-XXXXXXXXX-X",
+  },
+  {
+    id: "idType",
+    label: "ID Type",
+    type: "text",
+    placeholder: "Ghana Card",
+  },
+  {
+    id: "licenseNo",
+    label: "Driver License",
+    type: "text",
+  },
+  {
+    id: "city",
+    label: "City",
+    type: "text",
+    placeholder: "tema",
+  },
+];
+
+// export const productInputs = [
+//   {
+//     id: 1,
+//     label: "Title",
+//     type: "text",
+//     placeholder: "Apple Macbook Pro",
+//   },
+//   {
+//     id: 2,
+//     label: "Description",
+//     type: "text",
+//     placeholder: "Description",
+//   },
+//   {
+//     id: 3,
+//     label: "Category",
+//     type: "text",
+//     placeholder: "Computers",
+//   },
+//   {
+//     id: 4,
+//     label: "Price",
+//     type: "text",
+//     placeholder: "100",
+//   },
+//   {
+//     id: 5,
+//     label: "Stock",
+//     type: "text",
+//     placeholder: "in stock",
+//   },
+// ];
+
+export const hotelInputs = [
+  {
+    id: "name",
+    label: "Name",
+    type: "text",
+    placeholder: "toyota camry",
+  },
+  {
+    id: "type",
+    label: "Type",
+    type: "text",
+    placeholder: "sedan",
+  },
+  {
+    id: "city",
+    label: "City",
+    type: "text",
+    placeholder: "accra",
+  },
+  {
+    id: "desc",
+    label: "Description",
+    type: "text",
+    placeholder: "relaxed and smooth steering...",
+  },
+  {
+    id: "condition",
+    label: "Condition",
+    type: "text",
+    placeholder: "new",
+  },
+  {
+    id: "distance",
+    label: "Mileage",
+    type: "text",
+    placeholder: "50kmpl",
+  },
+  {
+    id: "title",
+    label: "Model",
+    type: "text",
+    placeholder: "toyota",
+  },
+  {
+    id: "vin",
+    label: "VIN",
+    type: "text",
+    placeholder: "vin serial",
+  },
+  {
+    id: "model",
+    label: "Make",
+    type: "text",
+    placeholder: "car make",
+  },
+  {
+    id: "year",
+    label: "Year",
+    type: "text",
+    placeholder: "2020",
+  },
+  {
+    id: "licensePlate",
+    label: "Car Number",
+    type: "text",
+    placeholder: "DV 2222 25",
+  },
+  {
+    id: "colour",
+    label: "Colour",
+    type: "text",
+    placeholder: "car colour",
+  },
+  {
+    id: "engine",
+    label: "Engine",
+    type: "text",
+    placeholder: "enigine capacity",
+  },
+  {
+    id: "transmission",
+    label: "Transmission",
+    type: "text",
+    placeholder: "manual",
+  },
+  {
+    id: "cheapestPrice",
+    label: "Price per day",
+    type: "text",
+    placeholder: "add in case of rental",
+  },
+  {
+    id: "sellingPrice",
+    label: "Selling Price",
+    type: "text",
+    placeholder: "2500000",
+  },
+  
+  
+];
+
+export const roomInputs = [
+  {
+    id: "title",
+    label: "Available for",
+    type: "text",
+    placeholder: "1-2 days, 3-4 weeks",
+  },
+  {
+    id: "name",
+    label: "Car",
+    type: "text",
+    placeholder: "toyota venza",
+  },
+  {
+    id: "desc",
+    label: "Description",
+    type: "text",
+    placeholder: "Available in accra only..",
+  },
+  {
+    id: "price",
+    label: "Price per day(ghc)",
+    type: "number",
+    placeholder: "100",
+  },
+  {
+    id: "maxPeople",
+    label: "Max People",
+    type: "number",
+    placeholder: "2",
+  },
+  {
+    id: "carNumber",
+    label: "Car Number",
+    type: "text",
+    placeholder: "GN 1122 24",
+  },
+  
+];
